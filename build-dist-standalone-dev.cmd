733d7110e2a82d8f0206d8e4b0f8d4bc4dd68a49
tbl0605 <tbl0605@gmail.com>
2025-02-28 17:31:59 +0100

    Fixes

--- build-dist-standalone-dev.cmd
+++ build-dist-standalone-dev.cmd
@@ -1,6 +1,6 @@
 pushd "%~dp0" || goto fin
 call "%ARP_PHP_CONF_83%\npm" install
-call "%ARP_PHP_CONF_83%\npx" update-browserslist-db@latest
+call "%ARP_PHP_CONF_83%\npx" update-browserslist-db@latest --update-db --yes
 call "%ARP_PHP_CONF_83%\npm" audit fix
 call "%ARP_PHP_CONF_83%\npm" prune
 call "%ARP_PHP_CONF_83%\npm" dedupe
